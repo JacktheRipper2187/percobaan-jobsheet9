8ba98f14f3021ca34aacf92d1276c0f9eb9139a3
JackTheRipper2187 <ulilf442@gmail.com>
2023-11-14 13:38:15 +0700

    modifikasi percobaan 3

--- ArrayNilai22.java
+++ ArrayNilai22.java
@@ -4,11 +4,16 @@ public class ArrayNilai22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiAkhir = new int[10];
+        int[] nilaiAkhir22 = new int[10];
 
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < nilaiAkhir22; i++){
+            if (nilaiAkhir22[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i+1) + " lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + (i+1) + );
+            }
             System.out.print("Masukkan nilai akhir ke-"+i+": ");
-            nilaiAkhir[i] = sc.nextInt();
+            nilaiAkhir22[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
             System.out.println("Nilai akhir ke-"+i+" adalah " +nilaiAkhir[i]);
--- ArrayRataNilai22.java
+++ ArrayRataNilai22.java
@@ -4,24 +4,38 @@ public class ArrayRataNilai22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] nilaiMhs= new int [10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa yang ingin dimasukkan = ");
+        int jumlahMhs = sc.nextInt();
         
-        for (int i = 0; i < nilaiMhs.length; i++){
-            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+        int[] nilaiMhs = new int[jumlahMhs];
+        int jumlahMhsLulus = 0, jumlahMhsTidak = 0, nilaiLulus = 0, nilaiTidak = 0, total = 0;
+        double rataLulus = 0, rataTidak = 0;
+
+        for (int i = 0; i < jumlahMhs; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
-        }
-         for (int i = 0; i < nilaiMhs.length; i++) {
+
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i+1) + " Lulus!");
+                jumlahMhsLulus++;
+                nilaiLulus += nilaiMhs[i];
+            } else {
+                System.out.println("Mahasiswa ke-" + (i+1) + " Tidak lulus!");
+                jumlahMhsTidak++;
+                nilaiTidak += nilaiMhs[i];
+            }
             total += nilaiMhs[i];
         }
-           rata2 = total/nilaiMhs.length;
-            System.out.println("Rata-rata nilai = "+rata2);
+
+        rataLulus = (double)nilaiLulus / (double)jumlahMhsLulus;
+        rataTidak = (double)nilaiTidak / (double)jumlahMhsTidak;
+
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahMhsLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahMhsTidak);
+
+        System.out.printf("Rata-rata nilai mahasiswa yang lulus = %.2f\n", rataLulus);
+        System.out.printf("Rata-rata nilai mahasiswa yang tidak lulus = %.2f\n", rataTidak);
         
-    
-    
-
-
+       
     }
-}    
-
+}
